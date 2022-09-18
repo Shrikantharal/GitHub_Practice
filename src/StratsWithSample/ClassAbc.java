bda0163d5ed717f69ba5e95927a00bd39f1a3cf8
Shrikant Haral <shrikantharal@gmail.com>
2022-09-18 18:07:36 +0530

    this is my first commit in git

---
+++ src/StratsWithSample/ClassAbc.java
@@ -0,0 +1,12 @@
+package StratsWithSample;
+
+public class ClassAbc {
+
+	public static void main(String[] args) {
+		
+		System.out.println("mai hoon don");
+		// TODO Auto-generated method stub
+
+	}
+
+}
